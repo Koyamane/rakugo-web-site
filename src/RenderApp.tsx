5464328386dda8d52d1745cbda1213cb6f137525
Koyamane <2662883144@qq.com>
2023-05-01 20:30:20 +0800

    UI: app的样式修改

--- src/RenderApp.tsx
+++ src/RenderApp.tsx
@@ -3,9 +3,10 @@
  * @Date: 2023-04-13 20:19:26
  * @LastEditors: dingyun
  * @Email: dingyun@zhuosoft.com
- * @LastEditTime: 2023-04-13 22:02:16
+ * @LastEditTime: 2023-05-01 20:29:47
  * @Description:
  */
+import { useEmotionCss } from '@ant-design/use-emotion-css'
 import { App, ConfigProvider } from 'antd'
 import { ReactNode } from 'react'
 
@@ -13,8 +14,13 @@ const RenderApp: React.FC<{ children: ReactNode }> = ({ children }) => {
   // 这个组件里面是拿不到主题 token 的，且只会走一次
   const color = localStorage.getItem('navTheme') === 'realDark' ? '#141414' : '#f5f5f5'
 
+  const appClassName = useEmotionCss(() => ({
+    height: '100%',
+    background: color
+  }))
+
   return (
-    <App style={{ height: '100%', background: color }} className='render-app'>
+    <App className={appClassName}>
       <ConfigProvider>{children}</ConfigProvider>
     </App>
   )
